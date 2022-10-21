4a540cff72a215c14534a7cf3fdb5815324b082e
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-20 21:19:06 -0700

    Authorize Profile

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,14 +1,28 @@
-const db = require ('../helpers/db')
-const table = "profile"; 
+const db = require('../helpers/db')
+const table = "profile";
 
-exports.insertProfile = (data) =>{
+exports.insertProfile = (data) => {
     const sql = `INSERT INTO "${table}" ("fullName", "birthDate", "picture", "userId") VALUES ($1, $2, $3, $4) RETURNING * `;
     const params = [data.fullName, data.birthDate, data.picture, data.userId];
     return db.query(sql, params);
-  };
+};
 
 exports.selectProfileByUserId = (id) => {
     const sql = `SELECT * FROM ${table} WHERE "userId" = $1`;
     const params = [id];
+    return db.query(sql, params);
+};
+
+exports.updateProfileByUserId = (id,data) => {
+    const column = Object.keys(data);
+    const val = Object.values(data);
+    
+    const conditionalSql = [];
+    column.forEach((col, i)=>{
+      conditionalSql.push(`"${col}"=$${2+i}`);
+    });
+  
+    const sql = `UPDATE "${table}" SET ${conditionalSql.join(", ")} WHERE "userId"=$1 RETURNING *`;
+    const params = [id, ...val];
     return db.query(sql, params);
   };
--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,8 +1,9 @@
 const profile = require ('express').Router()
 
 
-profile.get("/:id", require("../controllers/profile.controller").readProfileById);
 profile.get("/", require("../controllers/profile.controller").readProfileById);
+profile.get("/:id", require("../controllers/profile.controller").readProfileById);
+profile.put("/", require('../controllers/profile.controller').updateProfile);
 
 
 module.exports = profile
