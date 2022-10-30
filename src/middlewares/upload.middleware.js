b3e09ca43399081fd7ef806ed556b2d380186bd4
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-30 05:49:34 -0700

    upload back end

---
+++ src/middlewares/upload.middleware.js
@@ -0,0 +1,55 @@
+const multer = require("multer");
+const path =  require("path");
+
+const extGenerator = (mimeType) => {
+  const mime = ["image/jpeg", "image/png", "image/webp"];
+  const sortedExt = ["jpg", "png", "webp"];
+  return sortedExt[mime.indexOf(mimeType)];
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join("assets", "uploads"));
+  },
+  filename: async (req, file, cb) => {
+    const ext = extGenerator(file.mimetype);
+    const { customAlphabet } = await import("nanoid");
+    const nanoid = customAlphabet("0123456789",6);
+    
+    cb(null, nanoid().concat(`.${ext}`));
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(extGenerator(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("File extension not supported"), false);
+  }
+};
+
+const mult  = multer({ 
+  storage,
+  fileFilter,
+  limits : {
+    fileSize: 8 * 1000 * 1000
+  }
+});
+
+const upload = (field) => {
+  const up = mult.single(field);
+  return (req, res, next) => {
+    up(req, res, (err)=> {
+      if(err){
+        return res.status(400).json({
+          success: false,
+          message: err.message
+        });
+      } else {
+        next();
+      }
+    });
+  };
+};
+
+module.exports = upload;
--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,9 +1,10 @@
 const profile = require("express").Router();
 
 const {paramsUUID, basicUserCreds, check} =  require("../middlewares/profile.middleware");
+const upload = require("../middlewares/upload.middleware");
 
 profile.get("/", require("../controllers/profile.controller").readProfileById);
 profile.get("/:id", paramsUUID, check, require("../controllers/profile.controller").readProfileById);
-profile.put("/", basicUserCreds, check, require("../controllers/profile.controller").updateProfile);
+profile.put("/", upload("picture"), basicUserCreds, check, require("../controllers/profile.controller").updateProfile);
 
 module.exports = profile;
