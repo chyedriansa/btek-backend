3e04c759371d039b727ea1ddc8df29f5210021ba
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-31 20:31:20 -0700

    edit profile error picture

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,85 +1,52 @@
 const profileModel = require("../models/profile.model");
 
-exports.readProfileById = async(req, res)=> {
-  try {
-    const profile = await profileModel.selectProfileByUserId(req.params.id || req.userData.id);
-    if(profile.rowCount){
-      return res.json({
-        success: true,
-        message: "Profile user with id "+ req.params.id,
-        results: profile.rows[0]
-      });
-    }
-    return res.status(400).json({
-      success: false,
-      message: "User with id "+ req.params.id +" not found"
-    });
-  } catch(err) {
-    return res.status(500).json({
-      success: false,
-      message: "Error: "+err.message
-    });
-  }
-};
 
-exports.updateProfile = async(req, res) => {
+exports.readProfileById = async (req, res) => {
   try{
-    const profile = await profileModel.updateProfileById(req.userData.id, req.body);
-    if(profile.rowCount){
-      return res.json({
-        success: true,
-        message: "Update profile success",
-        results: profile.rows[0]
-      });
-    }
-  } catch(err) {
-    return res.status(500).json({
-      success: false,
-      message: "Error: "+err.message
-    });
-  }
-};
-
-exports.readProfileById = async(req, res)=> {
-  try {
+    console.log(req.userData.id);
     const profile = await profileModel.selectProfileByUserId(req.params.id || req.userData.id);
     if(profile.rowCount){
       return res.json({
-        success: true,
-        message: "Profile user with id "+ req.params.id,
-        results: profile.rows[0]
+        success : true,
+        message: "Profile user with id" +req.params.id,
+        result: profile.rows[0]
       });
     }
     return res.status(400).json({
       success: false,
-      message: "User with id "+ req.params.id +" not found"
+      message: "User with id "+req.params.id+" not Found"
     });
-  } catch(err) {
+  }catch(err){
     return res.status(500).json({
       success: false,
-      message: "Error: "+err.message
+      message: "Error : "+err.message
     });
   }
 };
 
-exports.updateProfile = async(req, res) => {
+exports.updateProfile = async (req,res) => {
   try{
     delete req.body.picture;
-    if(req.file) {
+    if(req.file){
+      // console.log(req.file);
       req.body.picture = req.file.filename;
     }
+
+    console.log(req.body);
+
     const profile = await profileModel.updateProfileById(req.userData.id, req.body);
     if(profile.rowCount){
       return res.json({
         success: true,
-        message: "Update profile success",
+        message: "Update Profile Success",
         results: profile.rows[0]
       });
     }
-  } catch(err) {
+  }catch(err){
     return res.status(500).json({
       success: false,
-      message: "Error: "+err.message
+      message: "Error : "+err.message
     });
   }
 };
+
--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,37 +1,39 @@
 const multer = require("multer");
-const path =  require("path");
+const path = require("path");
 
-const extGenerator = (mimeType) => {
+const extGenerator = (mimetype) => {
   const mime = ["image/jpeg", "image/png", "image/webp"];
   const sortedExt = ["jpg", "png", "webp"];
-  return sortedExt[mime.indexOf(mimeType)];
+  return sortedExt[mime.indexOf(mimetype)];
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join("assets", "uploads"));
   },
-  filename: async (req, file, cb) => {
+  filename: async(req, file, cb) => {
     const ext = extGenerator(file.mimetype);
+
     const { customAlphabet } = await import("nanoid");
-    const nanoid = customAlphabet("0123456789",6);
+    const nanoid = customAlphabet("0123456789", 10);
     
+
     cb(null, nanoid().concat(`.${ext}`));
   }
 });
 
 const fileFilter = (req, file, cb) => {
-  if(extGenerator(file.mimetype)) {
+  if(extGenerator(file.mimetype)){
     cb(null, true);
-  } else {
+  }else{
     cb(new Error("File extension not supported"), false);
   }
 };
 
-const mult  = multer({ 
+const mult = multer({
   storage,
   fileFilter,
-  limits : {
+  limits: {
     fileSize: 8 * 1000 * 1000
   }
 });
@@ -39,17 +41,17 @@ const mult  = multer({
 const upload = (field) => {
   const up = mult.single(field);
   return (req, res, next) => {
-    up(req, res, (err)=> {
+    up(req, res, (err)=>{
       if(err){
         return res.status(400).json({
           success: false,
           message: err.message
         });
-      } else {
+      }else{
         next();
       }
     });
-  };
+  }; 
 };
 
 module.exports = upload;
