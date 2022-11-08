cbb6d7897ac2da5c23bd77b8ea201a78b92b1be4
Cahaya Edriansa <chyedriansa@gmail.com>
2022-11-07 23:09:00 -0800

    add:supabase

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 
 const db = new Pool({
-  connectionString : "postgresql://postgres:chyedriansa5@localhost:5432/postgres?schema=public"
+  connectionString : "postgresql://postgres:chyedriansa5@db.vydylhtkymrtwcqqrrwb.supabase.co:5432/postgres?schema=public"
 });
 
 module.exports = db;
